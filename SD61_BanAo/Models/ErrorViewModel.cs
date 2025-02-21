b24920681c08caa9030db3da9d209b82e3119c3a
Linh Đinh Văn <dapancuaban123@gmail.com>
2025-02-21 22:41:30 +0700

    linh commit

---
+++ SD61_BanAo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD61_BanAo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
